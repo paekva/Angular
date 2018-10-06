fdcaa145096f49ff2980871edeb41d0f0421bf43
Kate Pavlova <paekva@yandex.ru>
2018-10-06 20:50:38 +0300

    changed the way of presenting dish detail

--- conFusion/src/app/app-routing/routes.ts
+++ conFusion/src/app/app-routing/routes.ts
@@ -4,10 +4,12 @@ import { MenuComponent } from '../menu/menu.component';
 import { HomeComponent } from '../home/home.component';
 import { AboutComponent } from '../about/about.component';
 import { ContactComponent } from '../contact/contact.component';
+import { DishdetailComponent } from '../dishdetail/dishdetail.component';
 
 export const routes: Routes = [
   { path: 'home',  component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'dishdetail/:id',     component: DishdetailComponent },
   { path: 'menu',     component: MenuComponent },
   { path: 'about',     component: AboutComponent },
   { path: 'contactus',     component: ContactComponent }
--- conFusion/src/app/menu/menu.component.ts
+++ conFusion/src/app/menu/menu.component.ts
@@ -10,13 +10,8 @@ import { DishService } from '../services/dish.service';
 export class MenuComponent implements OnInit {
 
   dishes: Dish[];
-  selectedDish: Dish;
   
   constructor(private dishService: DishService) { }
-
-  onSelect(dish: Dish){
-    this.selectedDish = dish;
-  }
 
   ngOnInit() {
     this.dishes = this.dishService.getDishes();
