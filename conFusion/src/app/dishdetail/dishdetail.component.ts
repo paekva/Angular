3e34fcfa438970eedd74bb5d8be0b3d2ca1b0cb3
Kate Pavlova <paekva@yandex.ru>
2018-10-13 11:48:15 +0300

    new buttons for easy access to different dish details(to prev and next dish)

--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
-import {Dish} from '../shared/dish';
+import { Dish } from '../shared/dish';
 import { DishService } from '../services/dish.service';
 
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dishdetail',
@@ -13,6 +15,9 @@ import { Location } from '@angular/common';
 export class DishdetailComponent implements OnInit {
 
   dish: Dish;
+  dishIds: string[];
+  prev: string;
+  next: string;
 
   constructor(
     private dishService: DishService,
@@ -21,13 +26,27 @@ export class DishdetailComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    let id = this.route.snapshot.params['id'];
-    this.dishService.getDish(id)
-      .subscribe((dish) =>  this.dish = dish);
+    //getting Id's of all dishes
+    this.dishService.getDishIds().subscribe(dishIds => this.dishIds = dishIds);
+
+    //following the changes in url params in router link (observable in router), when the change happened
+    //creating a new observable, that returns us a dish, according to a change of is in params
+    this.route.params.pipe(
+      switchMap((params: Params) => this.dishService.getDish(params['id']))
+      )
+    .subscribe(
+      dish => { this.dish = dish; this.setPrevNext(dish.id); }
+      );
   }
 
   goBack(): void {
     this.location.back();
   }
 
+  setPrevNext(dishId: string) {
+    // according to a current dis id - find previous and next dishes
+    const index = this.dishIds.indexOf(dishId);
+    this.prev = this.dishIds[(this.dishIds.length + index - 1) % this.dishIds.length];
+    this.next = this.dishIds[(this.dishIds.length + index + 1) % this.dishIds.length];
+  }
 }
