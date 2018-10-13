2cbfbbf1e6d0fec852f7003b37054ab28b27528a
Kate Pavlova <paekva@yandex.ru>
2018-10-13 17:13:12 +0300

    new validation for a contact form, using observable

--- conFusion/src/app/contact/contact.component.ts
+++ conFusion/src/app/contact/contact.component.ts
@@ -16,6 +16,35 @@ export class ContactComponent implements OnInit {
 
   contactType = ContactType;
 
+
+  formErrors = {
+    'firstname': '',
+    'lastname': '',
+    'telnum': '',
+    'email': ''
+  };
+
+  validationMessages = {
+    'firstname': {
+      'required':      'First Name is required.',
+      'minlength':     'First Name must be at least 2 characters long.',
+      'maxlength':     'FirstName cannot be more than 25 characters long.'
+    },
+    'lastname': {
+      'required':      'Last Name is required.',
+      'minlength':     'Last Name must be at least 2 characters long.',
+      'maxlength':     'Last Name cannot be more than 25 characters long.'
+    },
+    'telnum': {
+      'required':      'Tel. number is required.',
+      'pattern':       'Tel. number must contain only numbers.'
+    },
+    'email': {
+      'required':      'Email is required.',
+      'email':         'Email is not in a valid format.'
+    },
+  };
+
   constructor(
     private fb: FormBuilder
   ) { }
@@ -24,18 +53,47 @@ export class ContactComponent implements OnInit {
     this.createForm();
   }
 
-  createForm() {
+  createForm(): void {
     this.feedbackForm = this.fb.group({
-      firstname: ['', Validators.required],
-      lastname: ['', Validators.required],
-      telnum: [0, Validators.required],
-      email: ['', Validators.required],
+      firstname: ['', [Validators.required, Validators.maxLength(25), Validators.minLength(2)]],
+      lastname: ['', [Validators.required, Validators.maxLength(25), Validators.minLength(2)]],
+      telnum: [0, [ Validators.required, Validators.pattern]],
+      email: ['', [ Validators.required, Validators.email]],
       agree: false,
       contacttype: 'None',
       message: ''
     });
+
+    // Subscribibg to any change of the form elements
+    this.feedbackForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); 
   }
 
+  onValueChanged(data? : any){
+    if (!this.feedbackForm) { return; }
+    const form = this.feedbackForm;
+
+    //For every el in the form we ...
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // ... clear previous error message (if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        // state new error msg if needed
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          // with name of the error in mind we place necessary error msg
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+  }
   onSubmit() {
     this.feedback = this.feedbackForm.value;
     console.log(this.feedback);
